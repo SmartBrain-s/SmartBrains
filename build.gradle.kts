8eb13223e6015fecf525f10ca3b67742d1b591e6
Gustavo <gustavo.ccx@hotmail.com>
2025-02-28 08:57:05 -0300

    SMB-2
    - Criando conexão com o Banco de Dados
    - Alterando a Versão do Spring Boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.4.3"
+	id("org.springframework.boot") version "3.3.9"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "br.com.smartbrains"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.2"
 
 java {
 	toolchain {
@@ -19,20 +19,39 @@ configurations {
 	}
 }
 
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+	enabled = true
+	destinationDirectory.set(file("${projectDir}/dist"))
+}
+
+tasks.named<Jar>("jar") {
+	enabled = false
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
+
+	// Spring Boot Starters
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// Banco de Dados e Migração
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
+
+	// Lombok
+	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
+	// Desenvolvimento
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// Testes
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
