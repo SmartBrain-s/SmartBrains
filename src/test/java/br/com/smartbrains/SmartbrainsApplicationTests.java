db340e47534c175c0256de17f6b1ce595bd41d2f
Gustavo <gustavo.ccx@hotmail.com>
2025-02-27 23:33:09 -0300

    SMB-1
    - Commit Inicial

---
+++ src/test/java/br/com/smartbrains/SmartbrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartbrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
